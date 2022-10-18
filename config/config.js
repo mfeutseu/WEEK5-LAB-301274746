c05198146e1eea880d44216adca4283464b6599d
mfeutseu <mfeutseu@gmail.com>
2022-10-18 10:29:09 -0400

    Mongo Atlas setup completed

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI = "mongodb://127.0.0.1/media"
+//export const MongoURI = "mongodb://127.0.0.1/media"
+export const MongoURI = "mongodb+srv://mfeutseu:Korrea01!@cluster0.2cm3xni.mongodb.net/media?retryWrites=true&w=majority"
