15b9ff674c2fffec14082033f1889321001d4651
mfeutseu <mfeutseu@gmail.com>
2022-10-18 11:15:41 -0400

    password setup + login and registration ejs pages

--- app/app.js
+++ app/app.js
@@ -10,6 +10,17 @@ import {fileURLToPath} from 'url';
 import { lookup } from "dns";
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
+// Auth Step 1 - import modules
+import passport from "passport";
+import passportLocal from "passport-local"
+import flash from 'connect-flash'
+
+// Auth Step 2 - define our auth strategy
+let localStrategy = passportLocal.Strategy;
+
+// Auth Step 3 - import the user model
+import mongoose from "mongoose";
+
 // Import Mongoose Module
 import mongoose from "mongoose";
 
@@ -41,11 +52,28 @@ app.use(express.json());
 app.use(express.urlencoded({extend: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(_dirname, '../public')));
+
+// Auth Step 4 - Setup Express Session
 app.use(session({
     secret: Secret,
     saveUninitialized: false,
     resave: false
 }))
+
+// Auth Step 5 - Step Flash 
+app.use(flash());
+
+// Auth Step 6 - initialize Password and Session
+app.use(passport.initialize());
+app.use(passport.session);
+
+// Auth Step 7 - implement the Auth strategy 
+passport.use(User.createStrategy());
+
+// Auth Step8 - Setup serialization and deserialization
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(User.deserializeUser);
+
 
 // use Routes 
 app.use('/', indexRouter);
---
+++ app/models/user.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import passportLocalMongoose from 'passport-local-mongoose';
+const { passportLocalSchema } = mongoose;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    displayName: String,
+    username: String,
+    emailAdress: String,
+}, {
+        timestamps: true,
+        collection: 'users'
+    
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+export default mongoose.model('user', UserSchema);
